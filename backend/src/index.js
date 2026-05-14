7795ca46f68710edcc75a135a0f8147cc1f66a6d
Abhisdas <dashabhish@gmail.com>
2026-05-15 03:56:36 +0530

    web

--- backend/src/index.js
+++ backend/src/index.js
@@ -27,6 +27,16 @@ app.use(
   })
 );
 
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));  // add this line
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
